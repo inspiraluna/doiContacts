a7ea9c9597b9474af779f66212b0dbbec104ed1f
GOUABY Kalivogui <gouabyk14@yahoo.fr>
2019-12-25 17:21:31 +0500

    implemented a alert message when deleting a contact or a wallet

--- doi_react_app/src/components/WalletList.js
+++ doi_react_app/src/components/WalletList.js
@@ -9,11 +9,28 @@ import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from '@material-ui/icons/Delete'
 import DetailsIcon from '@material-ui/icons/Details';
 import List from "@material-ui/core/List";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 
 const WalletList = ({checked}) => {
 
     const global = useGlobal()
     const [ wallets, setWallets ] = useGlobal('wallets')
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+        const currentGlobal = global;
+       currentGlobal.modus = 'list'
+       setGlobal(currentGlobal)
+    };
 
    const handleDetail = (index) => {
        const currentGlobal = global;
@@ -36,6 +53,9 @@ const WalletList = ({checked}) => {
         const currentWallets = wallets
         currentWallets.splice(index, 1);
         setWallets(currentWallets)
+        const currentGlobal = global;
+        currentGlobal.modus = 'list'
+        setGlobal(currentGlobal)
     }
 
     const ourWallets = wallets?wallets:[]//global.wallets?global.wallets:[]
@@ -57,8 +77,29 @@ const WalletList = ({checked}) => {
                     <IconButton onClick={() => handleEdit(index)} edge="end" aria-label="edit">
                         <DetailsIcon />
                     </IconButton>
-                    <IconButton edge="end" aria-label="delete" onClick={() => handleRemove(index)}>
+                    <IconButton edge="end" aria-label="delete" onClick={handleClickOpen}>
                         <DeleteIcon />
+                        <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Are you sure?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Do you really want to delete this wallet? this process cannot be undone
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleClose()} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={() => handleRemove(index)} color="primary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
                     </IconButton>
                 </ListItemSecondaryAction>
             </ListItem>)
