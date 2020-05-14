18aba4546afa15a401312e77c29fb00fb571bea2
Nico Krause <nico@le-space.de>
2020-05-14 18:40:20 +0200

    neue release v0.0.48

--- doi_react_app/src/utils/ipfs.js
+++ doi_react_app/src/utils/ipfs.js
@@ -1,24 +1,32 @@
 console.log('starting ipfs...')
 const IPFS = require('ipfs')
-const all = require('it-all')
+const { globSource } = IPFS
+const CID = require('cids')
+//const all = require('it-all')
 
 async function main () {
     const node = await IPFS.create()
     const version = await node.version()
 
-    console.log('Version:', version.version)
+   console.log('Version:', version.version)
 
-    for await (const file of await node.add({
-        path: 'hello.txt',
-        content: Buffer.from('Hello World 101')
-    })) {
-        console.log('Added file:', file.path, file.cid.toString())
-
-        const data = Buffer.concat(await all(node.cat(file.cid)))
-
-        console.log('Added file contents:', data.toString())
-        return
+    let lastCID
+    for await (const file of node.add(globSource('./build/', { recursive: true }))) {
+        lastCID = file.cid
+        console.log(file)
     }
+
+    // The address of your files.
+    const addr = '/ipfs/'+lastCID.toString() //Qmb3QrU3xgx3H3F3BgqLCQRwkohuseTFjNLYZmdV2Tq37H'
+    console.log("addr",addr)
+    const res = await node.name.publish(addr)
+// You now have a res which contains two fields:
+//   - name: the name under which the content was published.
+//   - value: the "real" address to which Name points.
+    console.log(res)
+    console.log(`https://gateway.ipfs.io/ipns/${res.name}`)
+
+
 }
 
 main()
